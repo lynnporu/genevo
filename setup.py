e140d0bea95c245772ec3666f9a32ddd1b0654c0
Paul Lynn <p.tymoshen@gmail.com>
2021-12-07 16:14:09 +0200

    Add setup manifest

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+with open("README.md", mode="r", encoding="utf-8") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="genevo",
+    version="0.0.1",
+    author="Pavlo Tymoshenko",
+    author_email="p.tymoshen@gmail.com",
+    description="Framework for evolutional neural networks",
+    long_description=long_description,
+    python_requires=">=3.5",
+    long_description_content_type="text/markdown",
+    url="https://github.com/lynnporu/genevo",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+    package_dir={"": "genevo"},
+    packages=setuptools.find_packages(where="genevo")
+)
