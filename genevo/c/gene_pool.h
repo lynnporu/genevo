2f9a2e8c70ad8a6937231ebdf0f58deb5863046c
Paul Lynn <p.tymoshen@gmail.com>
2021-12-09 04:25:06 +0200

    Move preambles declarations into pickler

--- genevo/c/gene_pool.h
+++ genevo/c/gene_pool.h
@@ -2,22 +2,15 @@
 
 #include <stdint.h>
 
+#include "pickler.h"
 #include "files.h"
 
-
-// Even the empty pool file should be at least 64 bits long.
-#define POOL_FILE_MIN_SAFE_BIT_SIZE 64
-
-#define POOL_INITIAL_BYTE           (uint8_t) 0xAB
-#define POOL_META_INITIAL_BYTE      (uint8_t) 0xBC
-#define POOL_META_TERMINAL_BYTE     (uint8_t) 0xCD
-#define POOL_TERMINAL_BYTE          (uint8_t) 0xFF
-
-#define GENOME_INITIAL_BYTE         (uint8_t) 0xA0
-#define GENOME_META_INITIAL_BYTE    (uint8_t) 0xDE
-#define GENOME_META_TERMINAL_BYTE   (uint8_t) 0xEF
-#define GENOME_RESIDUE_BYTE         (uint8_t) 0xA2
-#define GENOME_TERMINAL_BYTE        (uint8_t) 0xA1
+#define GENE_OUTCOME_IS_INPUT        0b10000000
+#define GENE_OUTCOME_IS_INTERMEDIATE 0b01000000
+#define GENE_OUTCOME_IS_OUTPUT       0b00100000
+#define GENE_INCOME_IS_INPUT         0b00010000
+#define GENE_INCOME_IS_INTERMEDIATE  0b00001000
+#define GENE_INCOME_IS_OUTPUT        0b00000100
 
 /*
 
@@ -54,14 +47,9 @@ This number can be increased with maximizing the number of bits.
 
 */
 
-#define GENE_OUTCOME_IS_INPUT        0b10000000
-#define GENE_OUTCOME_IS_INTERMEDIATE 0b01000000
-#define GENE_OUTCOME_IS_OUTPUT       0b00100000
-#define GENE_INCOME_IS_INPUT         0b00010000
-#define GENE_INCOME_IS_INTERMEDIATE  0b00001000
-#define GENE_INCOME_IS_OUTPUT        0b00000100
-
 typedef uint8_t gene_byte_t;
+#define GENE_BYTE_SIZE sizeof(gene_byte_t)
+
 typedef struct gene_s {
     uint64_t outcome_node_id;
     uint64_t income_node_id;
@@ -77,44 +65,6 @@ typedef struct gene_s {
     double   weight;           // weight is normalized to [-1; 1]
 } gene_t;
 
-/*
-
-Structure of the genome in the memory is the following:
-
-Content                               Size         Note
-----------                            ----------   ----------
-GENOME_INITIAL_BYTE                   8            Start byte of the genome
-[number of genes]                     32
-MNSB                                  16           Size of the metadata which
-                                                   describes this genome.
-GENOME_META_INITIAL_BYTE              8            Start byte of the metadata.
-MN                                    MNSb         The metadata itself
-GENOME_META_TERMINAL_BYTE             8            End byte of the metadata.
-[gene #1]                             2*IGSb + WGSb
-[gene #2]                             2*IGSb + WGSb
-...
-GENOME_RESIDUE_BYTE                   8            Genome residue byte
-[size of the residue in bits=RSb]     16           Size of the residual genome*.
-...                                   RSb          Residual genome.
-GENOME_TERMINAL_BYTE                  8            End byte of the genome
-
-* Maximum size of a gene in bits is 255*2 + 255 = 765. Just 10 bits is enough
-  To encode this number, but that would be unconvenient to put into struct, so
-  16 chosen instead.
-
-If the size of the genome cannot be aligned to size of the single gene (i.e.
-genome contains some terminal bits that are not anough to form the new gene),
-the residue can be placed after GENOME_RESIDUE_BYTE.
-
-*/
-
-typedef struct genome_preamble_s {
-    uint8_t   initial_byte;
-    uint32_t  genes_number;
-    uint16_t  metadata_byte_size;
-    uint8_t   metadata_initial_byte;
-} __attribute__((packed, aligned(1))) genome_file_preamble_t;
-
 typedef struct genome_s {
     uint32_t      length;
     uint8_t      *metadata;
@@ -123,49 +73,6 @@ typedef struct genome_s {
     uint16_t      residue_size_bits;
     uint8_t      *residue;
 } genome_t;
-
-/*
-
-Structure of the gene pool written in the file is the following:
-
-Content                               Size (bits)  Note
-----------                            ----------   ----------
-POOL_INITIAL_BYTE                     8            Initial byte used to verify
-                                                   integrity of the file
-[number of organisms]                 64
-[number of input neurons]             64
-[number of output neurons]            64
-[size of the OG and IG in bits]       8
-[size of the WG in bits]              8
-[size of metadata in bytes = MPSB]    16           Metadata contains desription
-                                                   of this gene pool
-POOL_META_INITIAL_BYTE                8            Start byte of the metadata
-MP                                    MPSb         The metadata itself
-POOL_META_TERMINAL_BYTE               8            End byte of the metadata
-[genome of the organism #1]           -
-[genome of the organism #2]           -
-...
-POOL_TERMINAL_BYTE                    8            End byte used to verify
-                                                   integrity of the file.
-
-! Note, that OG and IG has the same size.
-
-! Note, that size of OG in bits (OGSb) can be encoded with 8-bit number. That
-  means, maximum OGSb value can be 255. Maximum value for number, which consists
-  of 255 bits is 5.789604e+76, which is pretty large.
-
-*/
-
-typedef struct pool_file_preamble_s {
-    uint8_t  initial_byte;
-    uint64_t organisms_number;
-    uint64_t input_neurons_number;
-    uint64_t output_neurons_number;
-    uint8_t  node_id_part_bit_size;
-    uint8_t  weight_part_bit_size;
-    uint16_t metadata_byte_size;
-    uint8_t  metadata_initial_byte;
-} __attribute__((packed, aligned(1))) pool_file_preamble_t;
 
 typedef struct pool_s {
     uint64_t    organisms_number;
--- genevo/c/pickler.h
+++ genevo/c/pickler.h
@@ -16,9 +16,110 @@
 #include "error.h"
 #include "files.h"
 
-#define BYTE_SIZE sizeof(uint8_t)
+// Even the empty pool file should be at least 64 bits long.
+#define POOL_FILE_MIN_SAFE_BIT_SIZE 64
 
-#define IS_BIG_ENDIAN (!*(uint8_t *)&(uint16_t){1})
+#define POOL_INITIAL_BYTE           (uint8_t) 0xAB
+#define POOL_META_INITIAL_BYTE      (uint8_t) 0xBC
+#define POOL_META_TERMINAL_BYTE     (uint8_t) 0xCD
+#define POOL_TERMINAL_BYTE          (uint8_t) 0xFF
+
+#define GENOME_INITIAL_BYTE         (uint8_t) 0xA0
+#define GENOME_META_INITIAL_BYTE    (uint8_t) 0xDE
+#define GENOME_META_TERMINAL_BYTE   (uint8_t) 0xEF
+#define GENOME_RESIDUE_BYTE         (uint8_t) 0xA2
+#define GENOME_TERMINAL_BYTE        (uint8_t) 0xA1
+
+/*
+
+! The notation defined in gene_pool.h is used here. (So MNSb means the size of
+  metadata of a genome in bits, for example.)
+
+Structure of the genome in the memory is the following:
+
+Content                               Size         Note
+----------                            ----------   ----------
+GENOME_INITIAL_BYTE                   8            Start byte of the genome
+[number of genes]                     32
+MNSB                                  16           Size of the metadata which
+                                                   describes this genome.
+GENOME_META_INITIAL_BYTE              8            Start byte of the metadata.
+MN                                    MNSb         The metadata itself
+GENOME_META_TERMINAL_BYTE             8            End byte of the metadata.
+[gene #1]                             2*IGSb + WGSb
+[gene #2]                             2*IGSb + WGSb
+...
+GENOME_RESIDUE_BYTE                   8            Genome residue byte
+[size of the residue in bits=RSb]     16           Size of the residual genome*.
+...                                   RSb          Residual genome.
+GENOME_TERMINAL_BYTE                  8            End byte of the genome
+
+* Maximum size of a gene in bits is 255*2 + 255 = 765. Just 10 bits is enough
+  To encode this number, but that would be unconvenient to put into struct, so
+  16 chosen instead.
+
+If the size of the genome cannot be aligned to size of the single gene (i.e.
+genome contains some terminal bits that are not anough to form the new gene),
+the residue can be placed after GENOME_RESIDUE_BYTE.
+
+*/
+
+typedef struct genome_preamble_s {
+    uint8_t   initial_byte;
+    uint32_t  genes_number;
+    uint16_t  metadata_byte_size;
+    uint8_t   metadata_initial_byte;
+} __attribute__((packed, aligned(1))) genome_file_preamble_t;
+
+/*
+
+Structure of the gene pool written in the file is the following:
+
+Content                               Size (bits)  Note
+----------                            ----------   ----------
+POOL_INITIAL_BYTE                     8            Initial byte used to verify
+                                                   integrity of the file
+[number of organisms]                 64
+[number of input neurons]             64
+[number of output neurons]            64
+[size of the OG and IG in bits]       8
+[size of the WG in bits]              8
+[size of metadata in bytes = MPSB]    16           Metadata contains desription
+                                                   of this gene pool
+POOL_META_INITIAL_BYTE                8            Start byte of the metadata
+MP                                    MPSb         The metadata itself
+POOL_META_TERMINAL_BYTE               8            End byte of the metadata
+[genome of the organism #1]           -
+[genome of the organism #2]           -
+...
+POOL_TERMINAL_BYTE                    8            End byte used to verify
+                                                   integrity of the file.
+
+! Note, that OG and IG has the same size.
+
+! Note, that size of OG in bits (OGSb) can be encoded with 8-bit number. That
+  means, maximum OGSb value can be 255. Maximum value for number, which consists
+  of 255 bits is 5.789604e+76, which is pretty large.
+
+*/
+
+typedef struct pool_file_preamble_s {
+    uint8_t  initial_byte;
+    uint64_t organisms_number;
+    uint64_t input_neurons_number;
+    uint64_t output_neurons_number;
+    uint8_t  node_id_part_bit_size;
+    uint8_t  weight_part_bit_size;
+    uint16_t metadata_byte_size;
+    uint8_t  metadata_initial_byte;
+} __attribute__((packed, aligned(1))) pool_file_preamble_t;
+
+/*
+
+All data structures is being dumped into the file using using network byte
+order which is big-endian.
+
+*/
 
 #define hton16 htons
 #define ntoh16 ntohs
