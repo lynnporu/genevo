536545bdcd5a98c0b37d2a1688149f879acb8b78
Paul Lynn <p.tymoshen@gmail.com>
2022-01-02 21:29:12 +0200

    Throw ERR_GENE_NOT_ALIGNED in create_pool_in_file

--- genevo/c/demiurge.c
+++ genevo/c/demiurge.c
@@ -301,9 +301,19 @@ population_t * create_pool_in_file(
 	pool->output_neurons_number = output_neurons_number;
 	pool->node_id_part_bit_size = node_id_bit_size;
 	pool->weight_part_bit_size = weight_bit_size;
+
+	uint8_t gene_bit_size =
+		pool->node_id_part_bit_size * 2 + pool->weight_part_bit_size;
+
+	if (gene_bit_size % 8 /*!= 0*/) {
+		ERROR_LEVEL = ERR_GENE_NOT_ALIGNED;
+		destroy_pool(pool, false /*close file*/);
+		return NULL;
+	}
+
 	pool->gene_bytes_size = BITS_TO_BYTES(gene_bit_size);
 
-	genome_length_t genes_number = genome_bit_size / (pool->gene_bytes_size * 8);
+	genome_length_t genes_number = genome_bit_size / gene_bit_size;
 
 	genome_t ** const genomes = allocate_genome_vector(
 		organisms_number, false /* allocate data  */,
